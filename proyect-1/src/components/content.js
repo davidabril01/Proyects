56c5a3e153dbf6cfcd88a488524d2515c16af5ec
davidabril01 <davidabrilperrig@gmail.com>
2023-08-16 18:46:48 -0300

    llogivet

---
+++ proyect-1/src/components/content.js
@@ -0,0 +1,13 @@
+import Carrusel from "./carousel";
+import MenuGeneral from "./menu-general";
+import "./content.css"
+
+function Content() {
+    return (
+    <div>
+        <Carrusel></Carrusel>
+        <MenuGeneral></MenuGeneral>
+    </div>
+    );
+}
+export default Content;
---
+++ proyect-1/src/components/menu-convencional.js
@@ -0,0 +1,27 @@
+import "./menu-convencional.css"
+function MenuConvencional() {
+    return (
+        <div className="menu">
+            <nav className="menu__barra">
+                <ul className="menu__options">
+                    <li className="menu__option"><a href="#">Categorias</a></li>
+                    <li className="menu__option"><a href="#">Mapa</a></li>
+                    <li className="menu__option"><a href="#">Comida</a></li>
+                    <li className="menu__option"><a href="#">Perritos</a></li>
+                    <li className="menu__option option__gatitos">
+                        <a href="#">
+                            <span className="tooltiptext">
+                                <ul>
+                                    <li><a href="#">Opción 1</a></li>
+                                    <li><a href="#">Opción 2</a></li>
+                                    <li><a href="#">Opción 3</a></li>
+                                </ul>
+                            </span>Gatitos
+                        </a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    );
+}
+export default MenuConvencional;
